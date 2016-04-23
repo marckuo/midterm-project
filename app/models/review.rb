c74877904fe20cfd2df5e57417b88819e0c96a9e
achandi <chandi.ajit@gmail.com>
2016-04-23 12:25:29 -0400

    ratyrate yall

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,14 @@
 class Review < ActiveRecord::Base
 
-  belongs_to :user
   validates  :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, only_integer: true }
 
+  def reviewed_by_player
+    User.find_by(id: from_user_id)
+  end
+
+  def reviewed_player
+    User.find_by(id: to_user_id)
+  end
+
 end
+
